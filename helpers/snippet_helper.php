ed8ee814328964b1e14c0889cd2be115d82aa26f
Ryan Brodkin <ryan@glabstudios.com>
2011-02-25 22:37:12 -0800

    Added helper function to extract domain from URL.

--- helpers/snippet_helper.php
+++ helpers/snippet_helper.php
@@ -28,6 +28,18 @@ function method_name () {
 	return ucwords(method_clean($CI->router->fetch_method()));
 }
 
+function domain_filter ($url) {
+	
+	if (!$url) return FALSE;
+	
+	$host = parse_url($url, PHP_URL_HOST);
+	$host = array_reverse(explode('.',$host));
+	
+	if ($host[0] == 'uk') return $host[2].'.'.$host[1].'.'.$host[0]; 
+	else return $host[1].'.'.$host[0];
+	
+}
+
 function greeting () {
 	$hour = date("H");
 	
@@ -38,6 +50,28 @@ function greeting () {
 	} else { 
 		return "Good Evening"; 
 	} 
+}
+
+function google_map ($size, $coordinates) {
+	
+	if (func_num_args() < 1) return FALSE;
+	
+	$params['size'] = $size;
+	//$params['maptype'] = 'terrain';
+	$params['sensor'] = 'false';
+	
+	$params['markers'] = null;
+	if (is_string($coordinates)) {	
+		$args = func_get_args();
+		unset($args[0]);
+		$coordinates = $args;
+	}
+	foreach ($coordinates as $id=>$coordinate) {
+		$params['markers'] .= $coordinate.'|';
+	}
+	
+	return "http://maps.google.com/maps/api/staticmap?".http_build_query($params);
+	
 }
 
 function pagenav ($actionName=null,$actionURI=null) {
