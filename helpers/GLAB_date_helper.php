1be8f9a5e8c49470591d3667c4031dcb4312fb32
Ryan Brodkin <ryan@glabstudios.com>
2011-04-15 23:13:46 -0700

    Updated date_relative() to accept string-based time formats. Updated date_user() to have the ability to suppress the time in output.

--- helpers/GLAB_date_helper.php
+++ helpers/GLAB_date_helper.php
@@ -1,6 +1,9 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 	function date_relative($time,$date_format='F j, Y') {
+		
+		if (!is_numeric($time)) $time = strtotime($time);
+		
 		$diff = time() - $time;
 		
 		if ($diff>0) {
@@ -38,7 +41,7 @@
 		}
 	}
 	
-	function date_user ($time,$return='Never') {
+	function date_user ($time, $show_time=true, $return='Never') {
 		
 		if (!$time) return $return;
 		if (!is_numeric($time)) $time = strtotime($time);
@@ -49,7 +52,7 @@
 		if (date('Y') === date('Y',$time) && date('n') === date('n',$time) && date('j') === date('j',$time)) $output = 'Today';
 		else $output = date('F j, Y',$time);
 		
-		if (date('H',$time) != 0 && date('i',$time) != 0) {
+		if ($show_time AND date('H',$time) != 0 AND date('i',$time) != 0) {
 			$output.= ' at ';
 			
 			if (isset($user['prefs']) && $user['prefs']['timeformat'] == 1) $output .= date('H:i T',$time);
