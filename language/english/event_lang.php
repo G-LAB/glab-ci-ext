c6a6a8bfc946f81416406e49cda178c972a21d4d
Ryan Brodkin <ryan@glabstudios.com>
2011-08-03 18:02:37 -0700

    Ported languages files out of CMS into CI-Extensions.

---
+++ language/english/event_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['event_auth_success'] = "%profile% logged in from %ip_address%.";
+$lang['event_auth_failure_mf_yubikey'] = "Failed login attempt via Yubikey (%error%) from %ip_address%.";
+
+?>
