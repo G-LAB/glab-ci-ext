d2a7597ccdb245ae80de251686d7503ddbfca760
Ryan Brodkin <ryan@glabstudios.com>
2011-11-03 20:02:12 -0700

    Adding more config items to Pagination.

--- config/pagination.php
+++ config/pagination.php
@@ -8,8 +8,14 @@ $config['query_string_segment'] = 'offset';
 $config['full_tag_open'] = '<span class="pagination">';
 $config['full_tag_close'] = '</span>';
 
-$config['next_link'] = '&raquo;';
-$config['prev_link'] = '&laquo;';
+$config['next_link'] = 'Next &gt;';
+$config['prev_link'] = '&lt; Previous';
+
+$config['last_link'] = 'Last &raquo;';
+$config['first_link'] = '&laquo; First';
+
+$config['cur_tag_open'] = '<span class="current">';
+$config['cur_tag_close'] = '</span>';
 
 /* End of file pagination.php */
 /* Location: ./system/application/config/pagination.php */ 
