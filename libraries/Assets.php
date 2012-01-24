2860a32f0ce97986cf8db71c528b1d129f21801d
Ryan Brodkin <ryan@glabstudios.com>
2012-01-23 21:13:48 -0800

    Asset loader now sends 304 status when files are not updated.

--- libraries/Assets.php
+++ libraries/Assets.php
@@ -27,11 +27,27 @@ abstract class Asset_loader extends CI_Controller
 
 		if (realpath($path) !== false)
 		{
+			$data = read_file($path);
+
+			if (php_sapi_name() == 'apache2handler' || php_sapi_name() == 'apache')
+			{
+			    $headers = apache_request_headers();
+
+			    if (isset($headers['If-Modified-Since']) && !empty($headers['If-Modified-Since']))
+			    {
+			        header('Not Modified',true,304);
+			        exit;
+			    }
+			}
+
 			header('Content-Type: '.get_mime_by_extension(basename($path)));
 			header('Cache-Control: max-age=3600, must-revalidate');
 			header('Last-Modified: '.standard_date('DATE_COOKIE',filemtime($path)));
-			echo read_file($path);
+
+			echo $data;
+
 			exit;
+
 		}
 		else
 		{
