9b294599d6c62efd826e5f2bf3cc8afd26bd0685
Ryan Brodkin <ryan@glabstudios.com>
2012-05-23 11:07:31 -0700

    Changed formatting of account numbers.

--- helpers/glab_number_helper.php
+++ helpers/glab_number_helper.php
@@ -1,12 +1,19 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-function acctnum_format($acctnum)
+function acctnum_format($str)
 {
-	if (empty($acctnum)) return '';
-	
-	$acctnum = preg_replace("/[^0-9]/", "", $acctnum);
-	
-	return preg_replace("/([0-9a-zA-Z]{4})([0-9a-zA-Z]{5})([0-9a-zA-Z]+)/", "$1-$2-$3", $acctnum);
+	// Strip Non-Digits
+	$str = preg_replace("/[^0-9]/", "", $str);
+
+	if (empty($str) === true)
+	{
+		return '';
+	}
+
+	// Pad Older PIDs to 16 Digits
+	$str = str_pad($str, 16, '00000000', STR_PAD_LEFT);
+
+	return preg_replace("/([\d]{4})([\d]{4})([\d]{5})([\d]+)/", "$1 $2 $3 $4", $str);
 
 }
 
